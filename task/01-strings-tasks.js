ef1eaf359a007fc9e48a4ad957e0d583dfc791f5
Sergey Streltsov <sergey.streltsov@100nuts.ai>
2019-04-13 23:42:05 +0300

    implement split e-mails in string

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -185,7 +185,8 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    // throw new Error('Not implemented');
+    return str.split(';');
 }
 
 /**
